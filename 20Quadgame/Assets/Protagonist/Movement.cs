6248fee33ea97eae30f9e969786f44519dfdf17d
Sam <samuel.stone@snhu.edu>
2020-05-02 18:20:17 -0400

    Started the player

---
+++ 20Quadgame/Assets/Protagonist/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public float playerJumpHeight = 2;
+    public GameObject ally;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("space"))
+        {
+            Instantiate(ally);
+        }
+        transform.position = pos;
+    }
+}
